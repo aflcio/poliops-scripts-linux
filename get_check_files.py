e3db66a8c56f768b89f0fc8be13e15748e892c9f
George Jansen <gjansen@aflcio.org>
2025-06-02 16:29:17 -0400

    Added logic to strip tabs from input fields before writing them to the tab-delimited file.

--- get_check_files.py
+++ get_check_files.py
@@ -16,7 +16,6 @@ import shutil
 import sys
 
 from pssh.clients import SSHClient
-import pyodbc
 import afl.dbconnections
 
 
@@ -35,7 +34,7 @@ class LocalSpec:
     """ Owing to the one-time flakiness of network shares, we make a practice
     of writing check files initially to a local filesystem, then copying them
     to a share from which Accounting can make use of them."""
-    
+
     temp_directory: str
     destination_directory: str
 
@@ -44,7 +43,7 @@ class LocalSpec:
 class Company:
     """ Poliops sends files in named cr-{company}.csv: the contents, if any
     we write to a pathname ending /COMPANY/{datetime}.tsv """
-    
+
     abbreviation: str
     directory_name: str
 
@@ -55,16 +54,17 @@ class FileSpec:
     based on company abbreviations, company names, etc. Rather than fool
     further with this, we supply a set of pathnames for each company, and
     pass that as needed. """
-    
+
     remote_path: str
     remote_path_old: str
     local_path: str
     share_path: str
 
+
 #
 # keys for our configuration.
 #
-    
+
 LOCAL = 'local'
 REMOTE = 'remote'
 DIRECTORY = 'directory'
@@ -91,6 +91,36 @@ FIELD_MAPPINGS = {'FCC': 'LM2',
                   'CommitID': 'COMMITID',
                   'RequestID': 'REQUESTID',
                   'PP Code': 'PROGRAM'}
+
+
+class RunRecorder:
+    """ Leave a trace behind us. """
+
+    def __init__(self) -> None:
+
+        conn = afl.dbconnections.connect('ga_helper')
+        self._cursor = conn.cursor()
+
+    def record_found_not_copied(self,
+                                pathname: str):
+        """ No need to pass in data records. """
+
+        self._cursor.execute("""INSERT INTO ga.poliops_transfers
+        (pathname, data_records, status, when_processed)
+        VALUES
+        (:pathname, 0, 'found but not copied', Sysdate)""",
+                             [pathname])
+
+    def record_copy(self,
+                    pathname: str,
+                    data_records: int):
+        """ One of few. """
+
+        self._cursor.execute("""INSERT INTO ga.poliops_transfers
+        (pathname, data_records, status, when_processed)
+        VALUES
+        (:pathname, :data_records, 'copied', Sysdate)""",
+                             [pathname, data_records])
 
 
 def copy_check_files(config_path: str,
@@ -126,7 +156,8 @@ def copy_check_files(config_path: str,
 
     def name_prospective_files(specs: dict[str, dict[str, str]],
                                companies: list[Company]) -> list[FileSpec]:
-        """ return a list of remote, local pairs. """
+        """ return a list of FileSpecs, giving paths at the remote and
+        local ends. """
 
         retval = []
         remote_directory = specs[REMOTE][DIRECTORY]
@@ -153,10 +184,11 @@ def copy_check_files(config_path: str,
 
     def available_non_empty_files(client: SSHClient,
                                   prospective_files: list[FileSpec],
+                                  recorder: RunRecorder,
                                   logger: logging.Logger) -> list[str]:
         temp = []
         for spec in prospective_files:
-            logging.debug('checking for', spec.remote_path)
+            logger.debug('checking for', spec.remote_path)
             host_out = client.run_command(f'ls {spec.remote_path}')
             for line in host_out.stdout:
                 temp.append(spec)
@@ -171,15 +203,18 @@ def copy_check_files(config_path: str,
                 logger.debug(
                     f'found {spec.remote_path} with at least one record')
             else:
+                recorder.record_found_not_copied(spec.remote_path)
                 logger.debug(f'{spec.remote_path} found but empty')
 
         return retval
 
     def rewrite_csv(ifh: io.TextIOBase,
-                    ofh: io.TextIOBase):
+                    ofh: io.TextIOBase) -> int:
         """ ifh is the output stream from ssh, which is giving us essentially
         a CSV text stream.
         ofh is the output stream, to write a tab-delimited file.
+        2025-06-02: make sure to strip out tabs from fields--a tab that slipped
+          in blew up the 2:15 PM  integration.
         """
 
         reader = csv.DictReader(ifh)
@@ -193,21 +228,23 @@ def copy_check_files(config_path: str,
                                 quotechar='|', escapechar='\\',
                                 lineterminator='\n')
         writer.writeheader()
-        for row in reader:
+        for i, row in enumerate(reader):
             row_out = {}
             for name in row:
                 if name in FIELD_MAPPINGS:
                     row_out[FIELD_MAPPINGS[name]] = row[name]
                 else:
-                    row_out[name] = row[name]
+                    row_out[name] = row[name].replace('\t', ' ')
             writer.writerow(row_out)
+
+        return i + 1
 
     def move_remote_files_aside(client: SSHClient,
                                 specs: list[FileSpec]) -> None:
         """ Put it in ~/cr/old, to be out of the way. """
 
         for spec in specs:
-            command_line = f'cp {spec.remote_path} {spec.remote_path_old}'
+            command_line = f'mv {spec.remote_path} {spec.remote_path_old}'
             print(f'running "{command_line}"')
             host_out = client.run_command(command_line)
             for line in host_out.stdout:
@@ -216,9 +253,10 @@ def copy_check_files(config_path: str,
             if exit_code != 0:
                 logging.warning(f'received code of {exit_code}'
                                 + f' on {command_line}')
-        
+
     def copy_files_to_host(client: SSHClient,
-                           file_specs: list[FileSpec]) -> list[str]:
+                           file_specs: list[FileSpec],
+                           recorder: RunRecorder) -> list[str]:
         """
          Copy the non-empty files, rewriting them from CSV to tab-delimited.
         """
@@ -227,17 +265,14 @@ def copy_check_files(config_path: str,
         for spec in file_specs:
             with open(spec.local_path, 'w') as ofh:
                 host_out = client.run_command(f'cat {spec.remote_path}')
-                rewrite_csv(host_out.stdout, ofh)
-                retval.append(spec.local_path)
-
+                records = rewrite_csv(host_out.stdout, ofh)
+                retval.append(spec)
+                recorder.record_copy(spec.remote_path, records)
         return retval
 
-    def copy_files_to_share(pathnames: list[str],
-                            final_dir: str):
-        for pathname in pathnames:
-            company, filename = os.path.split(pathname)[-2]
-            pathname2 = os.path.join(final_dir, company, filename)
-            shutil.copyfile(pathname, pathname2)
+    def copy_files_to_share(specs: list[FileSpec]) -> None:
+        for spec in specs:
+            shutil.copyfile(spec.local_path, spec.share_path)
 
     def setup_logger(debug: bool):
         logger = logging.getLogger(__name__)
@@ -245,20 +280,23 @@ def copy_check_files(config_path: str,
         logger.addHandler(handler)
         if debugging:
             logger.setLevel(logging.DEBUG)
-        
+
         return logger
 
+    recorder = RunRecorder()
     logger = setup_logger(debugging)
     config = get_configuration(config_path)
     client = make_ssh_client(config[REMOTE])
     prospective_files = name_prospective_files(config, COMPANIES)
     files_wanted = available_non_empty_files(client,
                                              prospective_files,
+                                             recorder,
                                              logger)
     logging.debug(f'we want {len(files_wanted)} files')
     if len(files_wanted) > 0:
-        pathnames = copy_files_to_host(client, files_wanted)
-        print(pathnames)
+        copied_files = copy_files_to_host(client, files_wanted,
+                                          recorder)
+        copy_files_to_share(copied_files)
     move_remote_files_aside(client, prospective_files)
 
 
